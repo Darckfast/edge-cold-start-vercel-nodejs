88997b4de6a763c24e7643e0b5b06636e41f78ce
darckfast <21337578+Darckfast@users.noreply.github.com>
2026-06-09 12:26:47 -0300

    fix: fixed .get on headers

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import { createServer } from 'node:http';
 
 const server = createServer((request, response) => {
-    const vercelId = request.headers.get('x-vercel-id');
+    const vercelId = request.headers['x-vercel-id'];
     response.writeHead(200, { 'Content-Type': 'application/json', 'x-datacenter': vercelId });
     response.end(JSON.stringify({ time: Date.now() }));
 });
