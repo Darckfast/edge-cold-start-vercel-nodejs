9ce0770178698c81c9a43fa0034248048a695cc4
darckfast <21337578+Darckfast@users.noreply.github.com>
2026-06-09 12:23:51 -0300

    first commit

---
+++ server.ts
@@ -0,0 +1,9 @@
+import { createServer } from 'node:http';
+
+const server = createServer((request, response) => {
+    const vercelId = request.headers.get('x-vercel-id');
+    response.writeHead(200, { 'Content-Type': 'application/json', 'x-datacenter': vercelId });
+    response.end(JSON.stringify({ time: Date.now() }));
+});
+
+server.listen(Number(process.env.PORT ?? 3000));
