e1847ca0b6ae96ae4844829552f4495ca3d7d3b4
darckfast <21337578+Darckfast@users.noreply.github.com>
2026-06-10 21:00:29 -0300

    fix: vercel nodejs does not accept micro

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import { createServer } from 'node:http';
 const server = createServer((request, response) => {
     const vercelId = request.headers['x-vercel-id'];
     response.writeHead(200, { 'Content-Type': 'application/json', 'x-datacenter': vercelId });
-    response.end(JSON.stringify({ time: Number(process.hrtime.bigint() / 1000n) }));
+    response.end(JSON.stringify({ time: performance.now() * 1000 }));
 });
 
 server.listen(Number(process.env.PORT ?? 3000));
