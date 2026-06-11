14182668053ba8e5326dd40f7330a0b007b5ead1
darckfast <21337578+Darckfast@users.noreply.github.com>
2026-06-10 21:35:44 -0300

    fix: replaced perf with date

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import { createServer } from 'node:http';
 const server = createServer((request, response) => {
     const vercelId = request.headers['x-vercel-id'];
     response.writeHead(200, { 'Content-Type': 'application/json', 'x-datacenter': vercelId });
-    response.end(JSON.stringify({ time: performance.now() * 1000 }));
+    response.end(JSON.stringify({ time: Date.now() * 1000 }));
 });
 
 server.listen(Number(process.env.PORT ?? 3000));
