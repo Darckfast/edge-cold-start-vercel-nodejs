a133e54c7132549f1556f93f0532f61509f79931
darckfast <21337578+Darckfast@users.noreply.github.com>
2026-06-10 21:36:00 -0300

    fix: removed old logic

--- server.ts
+++ server.ts
@@ -1,8 +1,7 @@
 import { createServer } from 'node:http';
 
 const server = createServer((request, response) => {
-    const vercelId = request.headers['x-vercel-id'];
-    response.writeHead(200, { 'Content-Type': 'application/json', 'x-datacenter': vercelId });
+    response.writeHead(200, { 'Content-Type': 'application/json' });
     response.end(JSON.stringify({ time: Date.now() * 1000 }));
 });
 
